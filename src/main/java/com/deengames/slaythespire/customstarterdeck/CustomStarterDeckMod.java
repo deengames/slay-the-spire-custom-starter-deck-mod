7264b87aa3c6211c88cea8f852407c3db1ee94ca
nightblade9 <nightbladecodes@gmail.com>
2023-06-09 17:54:48 -0400

    Progress! Game launches, shows selection, and grid goes berserk.

--- src/main/java/com/deengames/slaythespire/customstarterdeck/CustomStarterDeckMod.java
+++ src/main/java/com/deengames/slaythespire/customstarterdeck/CustomStarterDeckMod.java
@@ -1,22 +1,20 @@
 package com.deengames.slaythespire.customstarterdeck;
 
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.characters.AbstractPlayer.PlayerClass;
-import com.megacrit.cardcrawl.daily.mods.Insanity;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.daily.mods.SealedDeck;
-import com.megacrit.cardcrawl.daily.mods.Specialized;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ModHelper;
-import com.megacrit.cardcrawl.screens.custom.CustomMod;
+import com.megacrit.cardcrawl.random.Random;
+import com.megacrit.cardcrawl.unlock.UnlockTracker;
 
 import basemod.BaseMod;
-import basemod.interfaces.AddCustomModeModsSubscriber;
-import basemod.interfaces.EditCardsSubscriber;
 import basemod.interfaces.ISubscriber;
 import basemod.interfaces.PostCreateStartingRelicsSubscriber;
-import basemod.interfaces.PostDungeonInitializeSubscriber;
-import basemod.interfaces.PostDungeonUpdateSubscriber;
-import basemod.interfaces.PostInitializeSubscriber;
 import basemod.interfaces.PreDungeonUpdateSubscriber;
-import basemod.interfaces.PreUpdateSubscriber;
+import basemod.interfaces.PreStartGameSubscriber;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,8 +22,11 @@ import java.util.List;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
 
 @SpireInitializer
-public class CustomStarterDeckMod implements ISubscriber, PostDungeonInitializeSubscriber, PostInitializeSubscriber
-// PostCreateStartingRelicsSubscriber
+public class CustomStarterDeckMod implements ISubscriber, PostCreateStartingRelicsSubscriber, PreDungeonUpdateSubscriber
+//PreStartGameSubscriber
+//PostCreateStartingDeckSubscriber <--- really close, you pick character BUT too early to get current room
+//PostCreateStartingRelicsSubscriber
+//StartActSubscriber
 {
 	public static void initialize() {
         new CustomStarterDeckMod();
@@ -35,19 +36,37 @@ public class CustomStarterDeckMod implements ISubscriber, PostDungeonInitializeS
 		BaseMod.subscribe(this);
 	}
 
-
 	@Override
-	public void receivePostInitialize() {
+	public void receivePostCreateStartingRelics(PlayerClass arg0, ArrayList<String> arg1) {
+		// Set custom mode, we need it later
 		List<String> list = new ArrayList<String>();
 		list.add(SealedDeck.ID);
 		ModHelper.setMods(list);
 	}
 
+	// Pilfered from NeowEvent.dailyBlessing
+	private void doIt() {
+		CardGroup sealedGroup = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
+		
+		for (int i = 0; i < 30; i++) {
+			AbstractCard card = AbstractDungeon.getCard(AbstractDungeon.rollRarity());
+			if (!sealedGroup.contains(card)) {
+				sealedGroup.addToBottom(card.makeCopy());
+			} else {
+				i--;
+			} 
+		} 
+		
+		for (AbstractCard c : sealedGroup.group)
+		{
+			UnlockTracker.markCardAsSeen(c.cardID); 
+		}
+
+		AbstractDungeon.gridSelectScreen.open(sealedGroup, 10, CardCrawlGame.languagePack.getCharacterString("Neow Event").OPTIONS[4], false);
+	}
+
 	@Override
-	public void receivePostDungeonInitialize() {
-		List<String> list = new ArrayList<String>();
-		list.add(Specialized.ID);
-		list.add(SealedDeck.ID);
-		ModHelper.setMods(list);
+	public void receivePreDungeonUpdate() {
+		doIt();
 	}
 }
